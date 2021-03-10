ced97715a68d695d1c89b14d5ac0d4aeadaaa5a5
Nikolay Dutskinov - Musala <nikolay.dutskinov@musala.com>
2021-03-10 16:13:12 +0200

    Object movement & random seconds logic
    Create initial version of the game, where 3d object a cube falls from a certain place of the screen.
    The fall should be on a random principle, e.g 1-4 seconds.
    The fall should have speed that can be changed.
    Handle the tap event on the screen or the press of “w” for example.

---
+++ Catch the bill/Assets/ScoreText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public Text scoreText;
+
+    public void SetScoreText(string text)
+    {
+        scoreText.text = text;
+    }
+}
